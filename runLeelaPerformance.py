beb2a846d7fcb58997f311dfa42faaed3c4eef33
Veronica K. B. Olsen <v.k.b.olsen@cern.ch>
2019-03-15 19:50:22 +0100

    Updated run scripts.

---
+++ runLeelaPerformance.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*
+"""SixTrack Test Builds
+
+  SixTrack Tools - SixTrack Test Builds
+ =======================================
+  By: Veronica Berglyd Olsen
+      CERN (BE-ABP-HSS)
+      Geneva, Switzerland
+"""
+
+import sys
+import logging
+from sttb import *
+
+logger = logging.getLogger("sttb-logger")
+
+theConfig = Config(
+  workDir  = "/scratch/SixTrackTestBuilds",
+  jobsDir  = "/scratch/SixTrackTestBuilds/sync/jobs",
+  logLevel = "info"
+)
+theConfig.gitCheckout(sys.argv)
+
+##
+#  Debian Linux, 64 BIT
+##
+
+xDebian10 = Worker(theConfig, "Debian10")
+xDebian10.addCompiler("gfortran", "--version")
+xDebian10.addCompiler("ifort",    "--version")
+xDebian10.addCompiler("nagfor",   "-V")
+xDebian10.setLibDependencies(["naff","boinc"])
+
+xDebian10.addJob("Performance GNU",   "gfortran", "Release", "", "-E prob")
+xDebian10.addJob("Performance Intel", "ifort",    "Release", "", "-E prob")
+xDebian10.addJob("Performance NAG",   "nagfor",   "Release", "", "-E prob")
+
+xDebian10.writeJobFiles()
